842cb5664275ef3f2f9267bac33c0127aba603e3
Yasser Yousef <115915400+ammaryousef1@users.noreply.github.com>
2023-09-07 10:25:00 +0300

    First Commit

--- app/Component/Intro/Intro.js
+++ app/Component/Intro/Intro.js
@@ -1,89 +1,56 @@
 'use client'
 import React from 'react'
-import './Intro.scss'
- import linkedin from '../../assets/image/linkedin.png'
-import github from '../../assets/image/github.png'
+import glassesimoji from '../../assets/image/glassesimoji.png'
+import boy from '../../assets/image/boy.png'
 import Vector1 from '../../assets/image/Vector1.png'
 import Vector2 from '../../assets/image/Vector2.png'
-import boy from '../../assets/image/boy.png'
-import FloatingDiv from './FloatingDiv'
-import crown from '../../assets/image/crown.png'
-import thumbup from '../../assets/image/thumbup.png'
-import glassesimoji from '../../assets/image/glassesimoji.png'
-import { motion } from 'framer-motion'
 import Image from 'next/image'
-
+import FloatingDiv from './FloatingDiv'
+import Github from '../../assets/image/github.png'
+import Linkedin from '../../assets/image/Linkedin.png'
+import crown from '../../assets/image/crown.png'
+import { motion } from 'framer-motion'
 const Intro = () => {
-
   return (
-    <>
+    <div className='mt-36 flex'>
+      <div className='blur  relative '></div>
+      <div>
+        <h1 className='text-[55px] font-semibold text-black'>Hy! I Am</h1>
+        <h1 className='text-[55px] font-semibold text-yellow'>Ammar Yousef</h1>
+        <p  className='mt-3 text-[14px] font-semibold text-gray'>Frontend Developer with high level of experience in web development producting the Quality Work</p>
+        <button
+          className='mt-10 rounded-full bg-yellow p-3 px-7 shadow-lg shadow-orange text-white'>
+          Hire me
+        </button>
+        <div className='flex gap-9 relative top-20'>
+          <Image width={70} height={50}  src={Github} alt='' />
+          <Image width={70} height={50}  src={Linkedin} alt='' />
 
-    <div  className='intro'>
-        <div className="i-left">
-            <div className="i-name">
-                <span>Hy ! I Am </span>
-                <span>Ammar Yousef</span>
-                <span>Frontend Developer With level of experience in web designing and development producting the Quality work</span>
-            </div>
-            <button className="button i-button" ><a href="/contact" style={{ position: 'relative' , bottom: '3px'}} >Hire me</a></button>
-            <div className="i-icons">
-                <a href=''>
-                   <Image src={github} alt='' />
-                </a>
-                <a href='https://www.linkedin.com/in/ammar-yousef-559b51283/' target='__blank' >
-                   <Image src={linkedin} alt='' />
-                </a>
-            </div>
         </div>
-        <div className="i-right">
-            <Image  className='w-[1000px]' src={Vector1} alt="" />
-            <Image  className='w-[1000px]' src={Vector2} alt="" />
-            <Image src={boy} alt="" />
-            <motion.img
-            animate={{ x:100}}
-            
-            transition={{type: 'tween'  ,duration: 1}}
-            className='emoge'
-            style={{ width: '100px' , top:'10px'}}
-            src={glassesimoji}
-            alt="" />
-
-            <motion.div
-             animate={{ x:-50}}
-             transition={{type: 'tween'  ,duration: 1}}
-             style={{top:'-4%' , left: '68%'}}
-             className='floating-div'>
-                <FloatingDiv   image={crown} txt1="Web" txt2="Developer" />
-            </motion.div>
-
-             <motion.div
-             animate={{ x:50}}
-             transition={{type: 'tween'  ,duration: 1}}
-             style={{top: '25.5rem' , left: '0rem'}} 
-             className='floating-div'>
-                <FloatingDiv   image={thumbup} txt1="also Designing" txt2="" />
-            </motion.div>
-
-            <div className="blur s-blur" style={{     background: 'blue',
-                top: '17rem',
-                width: '11rem',
-                height: '11rrem',
-                top: '50px' , 
-                zIndex: '-10'
-
-                }}></div>
-            <div className="blur s-blur" style={{     background: 'blue',
-                top: '17rem',
-                width: '11rem',
-                height: '11rrem',
-                left: '-9rem',
-                position: 'relative',
-                zIndex: '-10'}}></div>
-
-            
-        </div>
+      </div>
+      <div className='flex justify-between'>
+        <motion.div
+          animate={{ x:50}}
+          transition={{type: 'tween'  ,duration: 1}}
+          style={{top:'-4%' , right: '50%'}}
+        >
+        <Image  className='w-[110px] h-[110px]' src={glassesimoji} alt='' />
+        </motion.div>
+        <div className='absolute'>
+        <Image className='relative top-20px w-[700px]' src={Vector1} alt='' />
+        <Image className='relative left-[110px] bottom-[500px] w-[600px]' src={Vector2} alt='' />
+        <Image className='relative bottom-[1110px] left-[160px] w-[450px]' src={boy} alt='' />
+        <motion.div
+         animate={{ x:-50}}
+         transition={{type: 'tween'  ,duration: 1}}
+         style={{top:'-4%' , left: '25%' }}
+         className="relative bottom-[1250px] right-[10px] ">
+         <FloatingDiv  image={crown} txt1="Web" txt2="Developer" />
+         <div className='blur '> </div>
+        </motion.div>
+        </div> 
+      </div>
     </div>
-    </>
   )
 }
 
