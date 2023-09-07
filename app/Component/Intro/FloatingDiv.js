29044d46c337faa0d6f6edd52ab0f5f7a847b4ff
Yasser Yousef <115915400+ammaryousef1@users.noreply.github.com>
2023-09-07 09:00:05 +0300

    first commit

--- app/Component/Intro/FloatingDiv.js
+++ app/Component/Intro/FloatingDiv.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
 
-const FloatingDiv = ({image , title , title1}) => {
+const FloatingDiv = ({image , txt1 , txt2}) => {
   return (
     <div className='bg-white  justify-center items-center rounded-2xl flex w-[200px] h-[100px] gap-2 '>
         <Image className='w-[50px] h-[50px]' src={image} alt='' />
         <div className=''>
-            <h1>{title}</h1>
-            <h1>{title1}</h1>
+            <h1>{txt1}</h1>
+            <h1>{txt2}</h1>
         </div>
     </div>
   )
