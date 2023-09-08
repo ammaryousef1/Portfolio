560b942823c081f171d2a93d2cf242e652c72d16
Yasser Yousef <115915400+ammaryousef1@users.noreply.github.com>
2023-09-08 10:03:10 +0300

    update data

---
+++ app/Component/Intro/IntroJs.js
@@ -0,0 +1,56 @@
+// 'use client'
+// import React from 'react'
+// import glassesimoji from '../../assets/image/glassesimoji.png'
+// import boy from '../../assets/image/boy.png'
+// import Vector1 from '../../assets/image/Vector1.png'
+// import Vector2 from '../../assets/image/Vector2.png'
+// import Image from 'next/image'
+// import FloatingDiv from './FloatingDiv'
+// import Github from '../../assets/image/github.png'
+// // import Linkedin from '../../assets/image/linkedin.png'
+// import crown from '../../assets/image/crown.png'
+// import { motion } from 'framer-motion'
+// const Intro = () => {
+//   return (
+//     <div className='flex flex-wrap items-start mt-36'>
+//       <div className='relative blur '></div>
+//       <div>
+//         <h1 className='text-[55px] font-semibold text-black'>Hy! I Am</h1>
+//         <h1 className='text-[55px] font-semibold text-yellow'>Ammar Yousef</h1>
+//         <p  className='mt-3 text-[14px] font-semibold text-gray'>Frontend Developer with high level of experience in web development producting the Quality Work</p>
+//         <button
+//           className='p-3 mt-10 text-white rounded-full shadow-lg bg-yellow px-7 shadow-orange'>
+//           Hire me
+//         </button>
+//         <div className='relative flex gap-9 top-20'>
+//           <Image width={70} height={50}  src={Github} alt='' />
+//           {/* <Image width={70} height={50}  src={Linkedin} alt='' /> */}
+
+//         </div>
+//       </div>
+//       <div className='flex justify-between'>
+//         <motion.div
+//           animate={{ x:50}}
+//           transition={{type: 'tween'  ,duration: 1}}
+//         >
+//         <Image  className='w-[120px] h-[120px] relative bottom-[60px]' src={glassesimoji} alt='' />
+//         </motion.div>
+//         <div className='absolute'>
+//         <Image className='relative top-20px w-[700px]' src={Vector1} alt='' />
+//         <Image className='relative left-[110px] bottom-[500px] w-[600px]' src={Vector2} alt='' />
+//         <Image className='relative bottom-[1110px] left-[160px] w-[450px]' src={boy} alt='' />
+//         <motion.div
+//          animate={{ x:-50}}
+//          transition={{type: 'tween'  ,duration: 1}}
+//          style={{top:'-4%' , left: '25%' }}
+//          className="relative bottom-[1250px] right-[10px] ">
+//          <FloatingDiv  image={crown} txt1="Web" txt2="Developer" />
+//          <div className='blur '> </div>
+//         </motion.div>
+//         </div> 
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Intro
--- app/Component/Navbar.js
+++ app/Component/Navbar.js
@@ -4,19 +4,19 @@ import Image from 'next/image'
 import Github from '../assets/image/github.png'
 const Navbar = () => {
   return (
-    <div className='flex m-5 mx-10 justify-between items-center'>
-        <div className='flex gap-3 items-center'>
-            <h1  className='font-bold text-xl'>Ammar</h1>
+    <div className='flex items-center justify-between m-5 mx-10'>
+        <div className='flex items-center gap-3'>
+            <h1 className='text-xl font-bold text-black'>Ammar</h1>
             <Toggle />
         </div>
         <div className='flex items-center gap-10'>
-            <ul className='flex gap-20'>
-                <li className=''>Home</li>
-                <li className=''>Skills</li>
-                <li className=''>Projects</li>
+            <ul className='flex gap-20 text-black'>
+                <li className='font-semibold text-black'>Home</li>
+                <li className='font-semibold text-black'>Skills</li>
+                <li className='font-semibold text-black'>Projects</li>
             </ul>
             <button 
-            className='rounded-full bg-yellow p-3 px-7 shadow-lg shadow-orange text-white'>
+            className='p-3 text-white rounded-full shadow-lg bg-yellow px-7 shadow-orange'>
             Contact
             </button>
             <div className='blur'></div>
